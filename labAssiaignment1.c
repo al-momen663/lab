d98a2fec17b8ff7fe4132216989d9ec3d5311d79
al-momen663 <al.momen.663@gmail.com>
2023-05-25 11:23:20 +0600

    lab report

---
+++ labAssiaignment1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main()
+{
+
+	// Declearing input variable
+	int a, b;
+	int choice; // declearing choice variable
+
+	printf("Input first number: ");
+	scanf("%d", &a); // getting input 1 from user
+
+	printf("Input secound number: ");
+	scanf("%d", &b); // getting input 2 from user
+
+	printf("Enter your choice (1 for addition and 2 for multiplication): ");
+	scanf("%d", &choice); // getting choice from user
+
+	switch (choice)
+	{
+	case 1:
+		printf("Addition of two numbers: %d\n", a + b); // processing data from choice and show output
+
+		break;
+	case 2:
+		printf("Multiplication of two numbers: %d\n", a * b); // processing data from choice and show output
+
+	default:
+		printf("0\n"); // default result
+		break;
+	}
+}
